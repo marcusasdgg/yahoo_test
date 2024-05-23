6e2d91fcbbf5922099b58c1042288b8619c50563
marcusasdgg <marcusolachan@icloud.com>
2024-05-23 21:08:01 +1200

    feat: example finished

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use reqwest::header::{HeaderValue, Iter};
+use reqwest::header::{HeaderValue, Iter, USER_AGENT};
 use tokio::main;
 use reqwest::*;
 use std::time::{self, Duration};
@@ -12,31 +12,36 @@ async fn main() -> Result<()>
     let multi_client = reqwest::Client::new();
     let response = multi_client.get("https://fc.yahoo.com");
     let s = response.send().await?;
-    let t : Vec<_> = s.headers().into_iter().take(s.headers().len()).collect();
-    let mut s = String::new();
-    for lines in t {
-       if lines.0 == "set-cookie"
-       {
-            let str = lines.1.to_str().unwrap();
-            for i in str.chars()
-            {
-                if i == ';'{
-                    break;
-                }
-                s.push(i);
-            }
-       }
+    let mut i = 1;
+    let mut cookie_str = String::new();
+    for sr in s.cookies()
+    {
+        println!("{i}th cokies are {:?}", sr);
+        i+=1;
+        cookie_str = format!("{}={}", sr.name(), sr.value());
     }
 
+    println!("cookie is {:?}", cookie_str);
+    let user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36";
+    let crumb_response = multi_client
+        .get("https://query2.finance.yahoo.com/v1/test/getcrumb")
+        .header(COOKIE, cookie_str.clone())
+        .header(USER_AGENT, user_agent)
+        .send()
+        .await?
+        .text()
+        .await?;
+    let mut final_get = String::new();
+    final_get = format!("{}{}","https://query2.finance.yahoo.com/v7/finance/quote?symbols=TSLA&crumb=", crumb_response);
+    let get_tsla = multi_client.get(final_get)
+    .header(COOKIE, cookie_str)
+    .header(USER_AGENT, user_agent)
+    .send() 
+    .await?
+    .text()
+    .await?;
 
-    //got the cookie yay.
-    println!("cookie found is {s}");
-
-    std::thread::sleep(Duration::from_secs(2));
-    let mut get_crumb = multi_client.get("https://query2.finance.yahoo.com/v1/test/getcrumb");
-    get_crumb = get_crumb.header(COOKIE,s);
-    let response2 = get_crumb.send().await?.text().await?;
-    println!("response is {:?}",response2);
+    println!("tesla is rawr {get_tsla}");
 
     Ok(())
 }
