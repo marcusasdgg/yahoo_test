9c838146e9c66a90f7fd566c81e83d22965487ce
marcusasdgg <marcusolachan@icloud.com>
2024-07-13 09:55:34 +1000

    a

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use yahoo_tick_grabber::YAHOOCONNECT;
 #[tokio::main]
 async fn main() -> Result<(), ()> {
     let s = YAHOOCONNECT::new().await.unwrap();
-    let guts = s.get_ticker("CXO.ax").await.unwrap();
+    let guts = s.get_ticker("RTX").await.unwrap();
     println!("{}", guts);
     let t = FinResult::new(&guts);
     for b in t.unwrap() {
